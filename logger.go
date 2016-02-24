12328da185cefa8cb906c82545a5285be6179e8c
Milan Bella <milanbella@hotmail.com>
2016-02-24 03:46:32 +0000

    logger setting

--- logger.go
+++ logger.go
@@ -1,14 +1,26 @@
 package gocosem
 
 import (
+	"io"
 	"log"
 	"os"
 )
 
+var errorLogWriter io.Writer = os.Stderr
+var debugLogWriter io.Writer = os.Stderr
+
 func getErrorLogger() *log.Logger {
-	return log.New(os.Stderr, "ERROR ", log.Ldate|log.Ltime|log.Lshortfile)
+	return log.New(errorLogWriter, "ERROR ", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 func getDebugLogger() *log.Logger {
-	return log.New(os.Stderr, "DEBUG ", log.Ldate|log.Ltime|log.Lshortfile)
+	return log.New(debugLogWriter, "DEBUG ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func SetErrorLogger(out io.Writer) {
+	errorLogWriter = out
+}
+
+func SetDebugLogger(out io.Writer) {
+	debugLogWriter = out
 }
