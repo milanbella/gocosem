4f198a85268f667826238c7a7977f14c104ae79f
TrueFurby <truefurby@gmail.com>
2016-03-30 11:42:11 +0200

    fix logging levels

--- main_test.go
+++ main_test.go
@@ -1,44 +1,20 @@
 package gocosem
 
-import (
-	"flag"
-	"os"
-	"testing"
-)
+import "flag"
 
 var (
-	realMeter bool = true
+	realMeter bool
 	meterIp   string
 )
 
 func init() {
-	var _logLevel string
+	Log.SetPrefix("")
 
+	var lvl string
 	flag.BoolVar(&realMeter, "real", false, "test against real meter")
 	flag.StringVar(&meterIp, "ip", "172.16.123.182", "meter ip address")
-	flag.StringVar(&_logLevel, "log", "INFO", "log level [ALL|DEBUG|FATAL|INFO|OFF]")
+	flag.StringVar(&lvl, "log", "INFO", "log level [ALL|TRACE|DEBUG|INFO|WARN|ERROR|FATAL|OFF]")
 	flag.Parse()
 
-	if "ALL" == _logLevel {
-		logLevel = LOG_LEVEL_ALL
-	} else if "DEBUG" == _logLevel {
-		logLevel = LOG_LEVEL_DEBUG
-	} else if "ERROR" == _logLevel {
-		logLevel = LOG_LEVEL_ERROR
-	} else if "FATAL" == _logLevel {
-		logLevel = LOG_LEVEL_FATAL
-	} else if "INFO" == _logLevel {
-		logLevel = LOG_LEVEL_INFO
-	} else if "OFF" == _logLevel {
-		logLevel = LOG_LEVEL_OFF
-	} else {
-		panic("incorrect value of command lone flag 'cosemLog': " + _logLevel)
-	}
-
-}
-
-func TestMain(m *testing.M) {
-	flag.Parse()
-	Log.SetPrefix("")
-	os.Exit(m.Run())
+	SetLogLevel(lvl)
 }
